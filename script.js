3f49e86db4eb6d64d1021a75dfe5f3c371b81f25
Dikaarsy <140580662+Dikaarsy@users.noreply.github.com>
2023-07-26 19:20:46 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+//Toggle class active
+
+const navbarNav = document.querySelector(".navbar-nav");
+
+//ketika humberger-menu di klik
+
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//klik diluar sidebar untuk menghilangkan nav
+
+const humberger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
